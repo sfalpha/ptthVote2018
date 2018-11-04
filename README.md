a46936468f016bef1393e9b2897d53a54dde3ba4
SF-Alpha <sfalpha@gmail.com>
2018-11-04 18:02:23 +0700

    Fixed vote overflow

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Solidity (Ethereum Smart Contract) Source Code for Phalung Thong Thin Thai Party Board Election 2018
 
 The voting data of accepting the Party Leader was replay to the rinkeby testnet on this contract
-https://rinkeby.etherscan.io/verifyContract2?a=0x77a91e08bb23cb8f5db6ea4156f1afddb8c8ec08
+https://rinkeby.etherscan.io/verifyContract2?a=0x1957a415ebcb9625b4af4ee610c211a1ecfbf5d8
--- ptthVote2018.sol
+++ ptthVote2018.sol
@@ -4,24 +4,17 @@ ptthVote2018
 
 Copyright (C) 2018 Phattanon Duangdara <sfalpha@gmail.com>
 
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
-"Software"), to deal in the Software without restriction, including
-without limitation the rights to use, copy, modify, merge, publish,
-distribute, sublicense, and/or sell copies of the Software, and to
-permit persons to whom the Software is furnished to do so, subject to
-the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 */
 
@@ -31,8 +24,8 @@ contract ptthVote2018 {
     
     struct Candidate {
         bool exist;
-        uint8 aye;
-        uint8 nay;
+        uint32 aye;
+        uint32 nay;
     }
 
     string public name;
@@ -75,12 +68,12 @@ contract ptthVote2018 {
         voters[msg.sender] = true;
     }
     
-    function getVoteAye(uint8 voteNumber)  public constant returns (uint8) {
+    function getVoteAye(uint8 voteNumber)  public constant returns (uint32) {
         require (candidates[voteNumber].exist);
         return candidates[voteNumber].aye;
     }
     
-    function getVoteNay(uint8 voteNumber) public constant returns (uint8) {
+    function getVoteNay(uint8 voteNumber) public constant returns (uint32) {
         require (candidates[voteNumber].exist);
         return candidates[voteNumber].nay;
     }
